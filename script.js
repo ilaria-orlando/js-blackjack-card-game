829c50861875295631a870de17464122df00f0fb
Ilaria Orlando <ilaria.orlando1991@gmail.com>
2020-11-19 14:22:17 +0100

    add list item through js  in HTML for every card that's pulled

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ let deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9
 let playercards = [];
 let total = 0;
 
-//take card random card from deck, push card in players deck and remove it from the main deck
+
+//take card random card from deck, push card in players deck and remove it from the main deck. Create list item for every card that's pulled
 pullCard = () => {
     let i = Math.floor(Math.random() * deck.length);
     let randomcard = deck[i];
+    let node = document.createElement("LI");
+    let textnode = document.createTextNode(randomcard);
     playercards.push(randomcard);
     deck.splice(i, 1);
+    node.appendChild(textnode);
+    document.getElementById("cards").appendChild(node);
 }
 
 //compare wining and losing conditions, change message inner html accordingly
@@ -25,9 +30,11 @@ winConditions = () => {
 }
 
 
+
 document.getElementById("test").addEventListener("click", function(){
     pullCard();
     winConditions();
+    document.getElementById("total").innerHTML = total;
     console.log(deck);
     console.log(playercards);
     console.log(total);
